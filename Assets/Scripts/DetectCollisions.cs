14a2156bdbb95b9ea3ba2a6daa78b1fe75f6fbda
Guilherme Pereira <guilhermecnpereira@tecnico.ulisboa.pt>
2024-04-20 16:43:35 +0100

    Oops

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -15,4 +15,10 @@ public class DetectCollisions : MonoBehaviour
     {
         
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
 }
